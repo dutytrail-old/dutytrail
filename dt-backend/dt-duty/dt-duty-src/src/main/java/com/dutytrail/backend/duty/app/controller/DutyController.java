1cee40c4b1e8ef5a51e3fed8f1977ca4dedb7450
Joan Tolós López <joan@Joans-MBP-2.mm-ads.com>
2019-11-28 16:44:04 +0100

    Dealing with common libs

--- dt-backend/dt-duty/dt-duty-src/src/main/java/com/dutytrail/backend/duty/app/controller/DutyController.java
+++ dt-backend/dt-duty/dt-duty-src/src/main/java/com/dutytrail/backend/duty/app/controller/DutyController.java
@@ -1,9 +1,11 @@
 package com.dutytrail.backend.duty.app.controller;
 
 import com.dutytrail.backend.duty.domain.service.DutyService;
+import com.dutytrail.backend.libs.HttpBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +19,9 @@ public class DutyController {
     private DutyService dutyService;
 
     @RequestMapping(value = "/ping", method = RequestMethod.GET)
-    public String ping() {
+    public ResponseEntity ping() {
         log.info("\t *** /ping endpoint called");
 
-        return this.dutyService.getPing();
+        return HttpBuilder.build(this.dutyService.getPing());
     }
 }
